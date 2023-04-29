7f6aa565bfaea52872f2e96807656ad8d2524d99
Cecilia <ceciliataboada1@gmail.com>
2023-04-29 17:02:14 -0300

    errors fixed

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,7 +1,7 @@
 function deleteNote(noteId){ /* This function sends a request to the backend */
     fetch("/delete-note", {
         method: "POST",
-        body: JSON.stringify({ noteID: noteId }) /* delete the note */
+        body: JSON.stringify({ noteId: noteId }), /* delete the note */
     }).then((_res) => {
         window.location.href = "/"; /* refresh the page */
     });
